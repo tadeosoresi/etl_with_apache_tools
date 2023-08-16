a75084974ebfa8d4c01e45cdab480fd9d47e4219
tadeo soresi <tadeosoresi@DESKTOP-Q20MPBG.localdomain>
2023-08-16 14:14:32 -0300

    UPDATED airflow and docker setp changed compose line

--- airflow_and_docker_setup.sh
+++ airflow_and_docker_setup.sh
@@ -2,8 +2,12 @@
 echo 'Updating system, installing Python'
 apt-get update && apt-get install -y python3-pip python3-dev python3-venv && apt-get -y install net-tools
 
+echo 'Launching docker services'
+docker compose up -d
+
 echo 'Creating virtual environment'
 python3 -m venv airflow_env
+
 echo 'Installing Airflow, setting up Airflow user'
 python3 . airflow_env/bin/activate && pip3 install --upgrade pip && \
 pip3 install 'apache-airflow[amazon]==2.4.2' \
@@ -15,8 +19,6 @@ pip3 install -r requirements.txt && airflow db init && airflow users create \
                                                                     --lastname ${AIRFLOW_LASTNAME} \
                                                                     --role Admin \
                                                                     --email ${AIRFLOW_EMAIL}
-echo 'Launching docker services'
-docker compose up -d
 echo 'ETL is UP! :)'
 
 # to run: sh airflow_and_docker_setup.sh
