ba94f844801b9372ba0d94ce1b8624becb72ffd3
tadeo soresi <tadeosoresi@DESKTOP-Q20MPBG.localdomain>
2023-08-23 17:15:58 -0300

    UPDATED etl_dag ADDED new tasks

--- airflow/dags/etl_dag.py
+++ airflow/dags/etl_dag.py
@@ -3,9 +3,11 @@ import sys
 import pendulum
 from datetime import datetime, timedelta
 from airflow import DAG
+from airflow.utils.trigger_rule import TriggerRule
 from airflow.operators.python import PythonOperator
 from airflow.providers.mongo.hooks.mongo import MongoHook
 from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.mongo.sensors.mongo import MongoSensor
 from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
 from airflow.providers.amazon.aws.transfers.mongo_to_s3 import MongoToS3Operator
 try:
@@ -15,16 +17,20 @@ except ModuleNotFoundError:
     sys.path.insert(1, path)
 from extraction.api_extraction import TMDBApiData
 
+date_of_execution = time.strftime("%Y-%m-%d")
+print(f'Date of execution: {date_of_execution}')
 def get_and_insert_data(mongo_conn_id, db, collection):
     """
     """
-    hook = MongoHook(conn_id='mongo_etl_id')
+    hook = MongoHook(conn_id=mongo_conn_id)
     client = hook.get_conn()
     db = client[db]
     collection = db[collection]
     print(f"Connected to MongoDB - {client.server_info()}")
     data_scraped = collection.find({}, {'_id': 0, 'id':1})
-    movies = TMDBApiData.get_data()
+    tmdb_class = TMDBApiData()
+    tmdb_class.date_of_scraping = date_of_execution
+    movies = tmdb_class.get_data()
     for movie in movies:
         if any(_dict['id'] == movie['id'] for _dict in data_scraped): continue
         collection.insert_one(movie)
@@ -45,10 +51,35 @@ with DAG(
         task_id='tmdb_api_to_local_mongo',
         python_callable=get_and_insert_data,
         op_kwargs={ # Las keys deben coincidir con los parametros de la funcion!! incluso en nombre
-            'mongo_conn_id': '',
+            'mongo_conn_id': 'mongo_etl_id',
             'db': 'tmdb_api',
             'collection': 'movies'
         },
+        dag=dag
+    )
+    task2 = MongoSensor(
+        collection='movies',
+        query={'created_at': date_of_execution},
+        mongo_conn_id='mongo_etl_id',
+        mongo_db='tmdb_api',
+        poke_interval=20,
+        timeout=480, 
+        trigger_rule=TriggerRule.ALL_DONE,
+        task_id="mongo_tmdb_data_sensor",
+        dag=dag
+    )
+    task2 = MongoToS3Operator(
+        mongo_conn_id='mongo_etl_id',
+        aws_conn_id='aws_etl_id',
+        mongo_collection='movies',
+        mongo_query={},
+        mongo_projection={'_id': 0},
+        s3_bucket='movies-datalake',
+        s3_key='tmdb_data/',
+        mongo_db='tmdb_api',
+        replace=True,
+        allow_disk_use=True,
+        compression='gzip',
         dag=dag
     )
     task1
