d2c6792555f60185788016e7f10f1951f4659197
tadeo soresi <tadeosoresi@DESKTOP-Q20MPBG.localdomain>
2023-09-01 12:41:18 -0300

    NEW hdfs dirs to creat sh files

---
+++ hdfs_dirs/create.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+hdfs dfs -mkdir -p /user/
+hdfs dfs -mkdir -p /user/local-datalake/
+hdfs dfs -mkdir -p /user/local-datalake/tmdb/
+hdfs dfs -mkdir -p /user/local-datalake/tmdb/movies/
+
