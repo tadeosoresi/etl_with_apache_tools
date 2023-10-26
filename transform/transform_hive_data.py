67cb5a5d8109b10b9f2af45cc21e28dcc8e15689
tadeosoresi <tadeosoresi23@outloo.com>
2023-10-26 19:47:33 -0300

    UPDATED transform_hive_data ADDED Hive configuration to merge schema

--- transform/transform_hive_data.py
+++ transform/transform_hive_data.py
@@ -7,6 +7,9 @@ spark = SparkSession.\
         appName("Hive_Data_To_HDFS") \
         .config("hive.metastore.uris", "thrift://172.103.0.9:9083") \
         .config("spark.sql.warehouse.dir", "/user/local-datalake/") \
+        .config("spark.sql.parquet.writeLegacyFormat", "true") \
+        .config("spark.sql.hive.convertMetastoreParquet", "false") \
+        .config("spark.sql.parquet.mergeSchema", "true") \
         .enableHiveSupport() \
         .getOrCreate()
 
@@ -17,15 +20,13 @@ df_exploded = df.select("id",
                      "media_type",
                      "original_language",
                      explode(col("cast")).alias("cast_exploded"))
-
 df_exploded = df_exploded.withColumn("actor", col("cast_exploded").getItem("original_name"))
-df_exploded = df_exploded.dropDuplicates(['id', 'title', 'actor'])
-df_exploded.drop(col("cast_exploded")) \
+df_exploded = df_exploded.drop(col("cast_exploded")).dropDuplicates(['id', 'title', 'actor'])
+df_exploded \
             .repartition(1) \
             .write.format("csv") \
             .mode("overwrite") \
             .option("sep", ",") \
             .option("header", "true") \
-            .option("compression", "snappy") \
-            .save("hdfs://172.103.0.17:9000/user/local-datalake/tmdb/movies.csv")
+            .save("hdfs://172.103.0.17:9000/user/local-datalake/tmdb/movies_neo_graph.csv")
 spark.stop()
